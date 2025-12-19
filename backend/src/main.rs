2cc546dc79e35dfce13c2cbb41ce9f64b73da32b
Michael Bidiak <sgame666@gmail.com>
2025-12-19 23:27:39 +0200

    Refactor main function and improve comments for clarity

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,24 @@
 use std::io;
 
 fn main() {
-    // Prompt the user for their name
     println!("Please enter your name:");
 
-    // Create a mutable String to hold the user input
     let mut user_name: String = String::new();
 
-    // Read the user's input from stdin
     io::stdin()
         .read_line(&mut user_name)
         .expect("Failed: Unable to read line.");
 
-    // Trim newline and print a greeting message
     let user_name = user_name.trim();
-    println!("Hello, world: {}!", user_name);
+    println!("Hello, : {}", user_name);
 
-    // Check if the user is "admin"
     if check_name(user_name) {
         println!("This is admin!");
     }
 }
 
-// Function to check if the user is "admin"
-// Return true if the user is "admin", otherwise false
+// Function to check if the user name is an admin name
+// Returns true if the name matches any of the predefined admin names
 fn check_name(user: &str) -> bool {
     matches!(
         user,
